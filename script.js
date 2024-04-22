3294e5171b73ac265f6e29aa2fbe74c5d9e0b106
LucaAgatinoPrivitera <priviteraluca01@gmail.com>
2024-04-22 15:52:22 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let dado1 = document.getElementById("dado-1");
+let dado2 = document.getElementById("dado-2");
+
+
+let numeroCasuale = Math.floor( Math.random()*6 );
+console.log(numeroCasuale)
+
+dado1.append(numeroCasuale);
+document.getElementById("dado-1").innerHTML = numeroCasuale;
+dado1.innerHTML = numeroCasuale;
+console.log(dado1)
+
+if (dado-1 > dado-2){
+
+}
