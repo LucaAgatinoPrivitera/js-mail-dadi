bf057a05929945d19f8312773993451283965235
LucaAgatinoPrivitera <priviteraluca01@gmail.com>
2024-04-22 17:31:34 +0200

    Non riesco a far funzionare il controllo completamente

--- script2.js
+++ script2.js
@@ -5,16 +5,30 @@ console.log(utentiRegistrati);
 console.log(utentiRegistrati.length, typeof utentiRegistrati.length);
 
 
-for (let c=0; c <= utentiRegistrati.length; c = c + 1) {
+for (let c=0; c <= utentiRegistrati.length; c++) {
     if (email == utentiRegistrati[c]) {
         console.log("Sei registrato");
-        document.getElementById("email").innerHTML = ("ciao");
+        document.getElementById("email").innerHTML = ("Sei iscritto");
+        // utentiRegistrati.push(email); Funziona in parte, scrive infinite volte "funziona"
     }
     else {
         console.log("Non sei registrato");
-        document.getElementById("email").innerHTML = ("no!");
+        document.getElementById("email").innerHTML = ("Non sei iscritto");
     }
 }
+
+
+// for (let c = 0; email != utentiRegistrati[c] && c <= utentiRegistrati.length; c++) {
+//     if (email == utentiRegistrati[c]) {
+//         console.log("Sei registrato");
+//         document.getElementById("email").innerHTML = ("Sei iscritto");
+//         // utentiRegistrati.push(email); Funziona in parte, scrive infinite volte "funziona"
+//     }
+//     else {
+//         console.log("Non sei registrato");
+//         document.getElementById("email").innerHTML = ("Non sei iscritto");
+//     }
+// }
 
 
 // Non funziona innerhtml con getElement con class
