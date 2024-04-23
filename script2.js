6061ad8e4e1d956f1f23d388ea68402a2a0f44c3
LucaAgatinoPrivitera <priviteraluca01@gmail.com>
2024-04-23 10:25:08 +0200

    Fix al codice, ora funziona come dovrebbe

--- script2.js
+++ script2.js
@@ -4,19 +4,29 @@ utentiRegistrati.push("funziona");
 console.log(utentiRegistrati);
 console.log(utentiRegistrati.length, typeof utentiRegistrati.length);
 
+let check;
 
-for (let c=0; c <= utentiRegistrati.length; c++) {
+for (let c = 0; c <= utentiRegistrati.length; c++) {
     if (email == utentiRegistrati[c]) {
-        console.log("Sei registrato");
-        document.getElementById("email").innerHTML = ("Sei iscritto");
+        // console.log("Sei registrato");
+        check = "Sei registrato";
+        // document.getElementById("email").innerHTML = ("Sei iscritto");
         // utentiRegistrati.push(email); Funziona in parte, scrive infinite volte "funziona"
     }
     else {
-        console.log("Non sei registrato");
-        document.getElementById("email").innerHTML = ("Non sei iscritto");
+        // console.log("Non sei registrato");
+        // document.getElementById("email").innerHTML = ("Non sei iscritto");
     }
 }
 
+if (check == "Sei registrato") {
+    document.getElementById("email").innerHTML = ("Sei iscritto");
+    console.log("Sei registrato");
+}
+else {
+    document.getElementById("email").innerHTML = ("Non sei iscritto");
+    console.log("Non sei registrato");
+}
 
 // for (let c = 0; email != utentiRegistrati[c] && c <= utentiRegistrati.length; c++) {
 //     if (email == utentiRegistrati[c]) {
